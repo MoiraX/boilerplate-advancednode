5a7cb17a41a8607af20f13a3fc9ca3d4b47b8594
MoiraX <moira.langley@gmail.com>
2022-07-10 12:50:40 +0200

    Registration of new users

--- server.js
+++ server.js
@@ -34,10 +34,9 @@ myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
   app.route('/').get((req, res) => {
-    res.render("pug/index.pug", {title: 'Connected to Database', message: 'Please login', showLogin: true});
+    res.render("pug/index.pug", {title: 'Connected to Database', message: 'Please login', showLogin: true, showRegistration: true});
   });
 
-  
   app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/profile');
   });
@@ -61,6 +60,37 @@ myDB(async client => {
       res.redirect('/');
     });
   });
+
+  app.route('/register')
+  .post((req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, function(err, user) {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
+            }
+          }
+        )
+      }
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
 
   app.use((req, res, next) => {
     res.status(404)
