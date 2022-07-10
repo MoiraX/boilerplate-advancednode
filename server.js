0fbb7749b87fd1fda179430fe1682567cf462ef1
MoiraX <moira.langley@gmail.com>
2022-07-10 10:12:09 +0200

    Serialisation of User Object

--- server.js
+++ server.js
@@ -4,22 +4,25 @@ const express = require('express');
 const myDB = require('./connection');
 const session = require('express-session');
 const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+app.set('view engine', 'pug')
 
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'pug')
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   cookie: { secure: false }
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -30,7 +33,19 @@ app.route('/').get((req, res) => {
   res.render("pug/index.pug", {title: 'Hello', message: 'Please login'});
 });
 
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  //myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  //});
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
+
+
